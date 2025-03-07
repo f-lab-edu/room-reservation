ed53657d1f3e0304d5ead0e2ed0631be0f908469
onerty <ownert24@gmail.com>
2025-03-07 17:44:47 +0900

    #15 feat: 누락된 UserRepository 쿼리 추가 (findByUserIdentifier)
    - 로그인 시 사용

--- src/main/java/com/onerty/yeogi/user/UserRepository.java
+++ src/main/java/com/onerty/yeogi/user/UserRepository.java
@@ -3,10 +3,14 @@ package com.onerty.yeogi.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByUserIdentifier(String email);
 
     boolean existsByNickname(String nickname);
+
+    Optional<User> findByUserIdentifier(String userIdentifier);
 }
