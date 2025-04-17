da039e796ae112996abea1ad5fa2250ea987a82a
onerty <ownert24@gmail.com>
2025-04-17 15:19:02 +0900

    refactor: 시큐리티 6.1.0 기준 설정 방식으로 변경
    - 스프링 시큐리티 6.1.0부터 체이닝 방식은 지양됨
    - 기존 메서드 체이닝 방식 대신 람다식으로 변경

--- yeogi-customer/src/main/java/com/onerty/yeogi/customer/security/SecurityConfig.java
+++ yeogi-customer/src/main/java/com/onerty/yeogi/customer/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -21,7 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf().disable()
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/**").permitAll() // 모든 API는 일단 허용
